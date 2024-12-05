88d8fef7b3881dc27a040f8478457a5d5e25b4a0
tikajeremias <tikajeremias@gmail.com>
2024-12-04 23:05:07 -0300

    backend update

--- backend/src/main/java/com/backend/shop_backend/ShopBackendApplication.java
+++ backend/src/main/java/com/backend/shop_backend/ShopBackendApplication.java
@@ -1,13 +1,13 @@
-package com.example.demo;
+package com.backend.shop_backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class ShopBackendApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(ShopBackendApplication.class, args);
 	}
 
 }
